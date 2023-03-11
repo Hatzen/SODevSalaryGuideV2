815c1610e9d4a9f1d3cac1de7af6c5c31196ca18
Hatzen <kaihartz@gmx.net>
2023-03-11 13:46:27 +0100

    add frontend gradle plugin to http modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,14 @@
+pluginManagement {
+    plugins {
+        kotlin("org.siouan.frontend-jdk11") version "6.0.0"
+    }
+    repositories {
+        gradlePluginPortal()
+    }
+}
+
 rootProject.name = "backend"
+
 include("adapter-http")
 include("adapter-persistence")
 include("adapter-batch")

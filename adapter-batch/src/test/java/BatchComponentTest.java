d77e009d952417e4163137a64955d574160e732d
Hatzen <kaihartz@gmx.net>
2023-03-29 17:40:16 +0200

    Trials setting up proper context

--- adapter-batch/src/test/java/BatchComponentTest.java
+++ adapter-batch/src/test/java/BatchComponentTest.java
@@ -5,16 +5,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 // https://stackoverflow.com/a/39215237/8524651
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 // https://stackoverflow.com/a/55838334/8524651
 @AutoConfigureTestDatabase
+@DataJpaTest
+@SpringBatchTest
+
 // @AutoConfigureWebClient
 // @AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
--- adapter-persistence/src/main/java/de/hartz/software/sodevsalaryguide/PersistenceConfiguration.java
+++ adapter-persistence/src/main/java/de/hartz/software/sodevsalaryguide/PersistenceConfiguration.java
@@ -1,14 +1,10 @@
 package de.hartz.software.sodevsalaryguide;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableAutoConfiguration
-@EnableJpaAuditing
 @Configuration
 @ComponentScan
 @EnableJpaRepositories(basePackages = {"de.hartz.software.sodevsalaryguide.repo"})

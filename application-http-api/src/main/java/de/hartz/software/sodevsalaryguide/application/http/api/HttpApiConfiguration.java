334a9493ec0c9b3709aa834e07a832b18dd78234
Hatzen <kaihartz@gmx.net>
2023-05-03 21:12:17 +0200

    working fronted

--- application-http-api/src/main/java/de/hartz/software/sodevsalaryguide/application/http/api/HttpApiConfiguration.java
+++ application-http-api/src/main/java/de/hartz/software/sodevsalaryguide/application/http/api/HttpApiConfiguration.java
@@ -1,5 +1,6 @@
 package de.hartz.software.sodevsalaryguide.application.http.api;
 
+import de.hartz.software.sodevsalaryguide.adapter.frontend.FrontendConfig;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;

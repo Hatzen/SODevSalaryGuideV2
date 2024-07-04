b80d2245d014033dd70c7b6b01c70db6fa96810a
Hatzen <kaihartz@gmx.net>
2024-07-04 12:44:29 +0200

    trial optimizing docker builds

--- src/main/env/init.sh
+++ src/main/env/init.sh
@@ -2,4 +2,5 @@
 set -e
 
 PROJECT_NAME="${1:-SoGuideLocal}"
+docker-compose build --no-cache --env-file .\example.env
 docker-compose -p $PROJECT_NAME --env-file .\example.env up -d --force-recreate

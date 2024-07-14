d9bebd285e8b3ea77b27cce878784384632048b7
Hatzen <kaihartz@gmx.net>
2024-07-14 13:48:51 +0200

    Fix some minor issues

--- application-http-frontend/src/main/frontend/src/services/stackOverflowCsvReader.ts
+++ application-http-frontend/src/main/frontend/src/services/stackOverflowCsvReader.ts
@@ -1,61 +1,27 @@
 import ResultSetForYear from '../model/resultSetForYear'
 import ControlState from '../model/controlState'
-// import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { RestLink } from 'apollo-link-rest';
-// import { gql } from '@apollo/client';
 import axios from 'axios';
-// const query = gql`
-//   query Luke {
-//     person @rest(type: "Person", path: "people/1/") {
-//       name
-//     }
-//   }
-// `;
-
-// const query = gql`
-//   query Luke {
-//     person @rest(type: "Person", path: "people/1/") {
-//       name
-//     }
-//   }
-// `;
 
 
 // https://medium.com/november-five/using-apollo-client-with-a-rest-api-ad0203a807cd
 export default class SurveyEntryService {
-    // TODO: Configure somehow via springs config server or reverse proxy
-    static readonly SERVER_URL = "http://ui-backend:8121"
+    get serverUrl(): string {
+        // TODO: Configure somehow via springs config server or reverse proxy
+        const isDebug = process.env.NODE_ENV === "development"
+        if (isDebug) {
+            return "http://localhost:8080"
+        }
+        return "http://ui-backend:8121"
+    }
 
-    // TODO: Implement GraphQL endpoints.
-    //restLink: RestLink
-    //// TODO: get rid of any..
-    //client: ApolloClient<any>
-
-    //constructor() {
-    //    this.restLink = new RestLink({ uri: SurveyEntryService.SERVER_URL + "/api/" });
-    //    this.client = new ApolloClient({
-    //        cache: new InMemoryCache(),
-    //        link: this.restLink
-    //      });
-    //}
     
     public getSurveyEntries(filter: ControlState): Promise<ResultSetForYear[]> {
-        return axios.post(SurveyEntryService.SERVER_URL + "/api/v1/participations/filtered", filter)
-        // Invoke the query and log the person's name
-        // return this.client.query({ query }).then(response => {
-        //     console.log(response.data.person.name);
-        //     return response.data
-        // });
+        return axios.post(this.serverUrl + "/api/v1/participations/filtered", filter)
     }
 
     
     public getFilterValues(): Promise<{countries: [string], abilities: [string], educations: [string]}> {
-        return axios.get(SurveyEntryService.SERVER_URL + "/api/v1/participations/values")
-        // Invoke the query and log the person's name
-        // return this.client.query({ query }).then(response => {
-        //     console.log(response.data.person.name);
-        //     return response.data
-        // });
+        return axios.get(this.serverUrl + "/api/v1/participations/values")
     }
 
 }
